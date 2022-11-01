e61ec3e16f57f9a0c8db63c480be5fc97eeec87d
hyeri na <107395229+hiy7030@users.noreply.github.com>
2022-11-01 20:08:26 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double A = s.nextInt();
+        double B = s.nextInt();
+
+        double result = A/B;
+        System.out.print(result);
+    }
+}
