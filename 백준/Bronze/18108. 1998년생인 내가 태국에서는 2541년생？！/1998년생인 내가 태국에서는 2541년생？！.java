267fbf6673d0a9e43112a6a0e52cc7170b240a83
hyeri na <107395229+hiy7030@users.noreply.github.com>
2022-11-01 20:09:25 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int y = s.nextInt();
+        int year = 543;
+
+        System.out.println(y-year);
+    }
+}
