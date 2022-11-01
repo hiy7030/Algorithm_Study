20d6d13266a9850cab79f3c147107131f39ed178
hyeri na <107395229+hiy7030@users.noreply.github.com>
2022-11-01 20:09:15 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        String wow = "??!";
+
+        System.out.print(str + wow);
+    }
+}
