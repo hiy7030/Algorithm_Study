47d0e852a71c34d09e4dbf28697bbb3c3347de38
hyeri na <107395229+hiy7030@users.noreply.github.com>
2022-11-05 19:57:01 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 208 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(Character.isUpperCase(c)) {
+                System.out.print(String.valueOf(c).toLowerCase());
+            } else {
+                System.out.print(String.valueOf(c).toUpperCase());
+            }
+        }
+    }
+}
