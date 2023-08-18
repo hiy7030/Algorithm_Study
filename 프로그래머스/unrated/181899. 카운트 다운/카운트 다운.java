5679c5d24107a8b9a68d4a11e45905b68987dc36
나혜리 <hiy7030@gmail.com>
2023-08-18 15:27:46 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start - end + 1];
+        
+        int i = 0;
+        
+        while(start >= end) {
+            answer[i] = start;
+            start--;
+            i++;
+        }
+        
+        return answer;
+    }
+}
