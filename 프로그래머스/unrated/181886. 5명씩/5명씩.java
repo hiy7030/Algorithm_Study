5746ce929e87fec705657a081972d95cfdade5c5
나혜리 <hiy7030@gmail.com>
2023-08-21 14:38:15 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> list = new ArrayList<>();
+        // 0 5 10 
+        for(int i = 0; i < names.length; i+=5) {
+            list.add(names[i]);
+        }
+        
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
