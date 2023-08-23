fc85eb442d07d3f37187daae275fe526d9e47198
나혜리 <hiy7030@gmail.com>
2023-08-23 12:42:35 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String binomial) {
+        int answer = 0;
+
+        String[] arr = binomial.split(" ");
+        int a = Integer.parseInt(arr[0]);
+        String op = arr[1];
+        int b = Integer.parseInt(arr[2]);
+
+        switch (op) {
+            case "+" : answer = a + b; break;
+            case "-" : answer = a - b; break;
+            case "*" : answer = a * b; break;
+        }
+
+        return answer;
+    }
+}
