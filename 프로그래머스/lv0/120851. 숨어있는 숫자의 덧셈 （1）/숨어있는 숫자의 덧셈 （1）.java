c376c9c6fd013451cc8c0baa8caf6e95672f6c4a
나혜리 <hiy7030@gmail.com>
2023-09-04 10:50:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.15 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        String str = my_string.replaceAll("[a-z,A-z]", "");
+        
+        int answer = 0;
+        
+        for(int i = 0; i < str.length(); i++) {
+            answer+= (str.charAt(i) - '0');
+        }
+        
+        return answer;
+    }
+}
