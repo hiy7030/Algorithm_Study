98dca670c168e096ded045726ffad687be457759
나혜리 <hiy7030@gmail.com>
2023-09-27 09:47:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        String num = String.valueOf(n);
+        int answer = 0;
+        
+        for(int i = 0; i < num.length(); i++) {
+            answer += (num.charAt(i) -'0');    
+        }
+        
+
+        return answer;
+    }
+}
