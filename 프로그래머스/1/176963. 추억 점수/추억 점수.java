f5dce95b2cda908c766599c910576534edeac2c5
나혜리 <hiy7030@gmail.com>
2023-10-13 09:37:35 +0900

    [level 1] Title: 추억 점수, Time: 0.20 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        Map<String, Integer> map = new TreeMap<>();
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for(int i = 0; i < photo.length; i++) {
+            for(int j = 0; j < photo[i].length; j++) {
+                if(map.containsKey(photo[i][j])) {
+                    answer[i] += map.get(photo[i][j]);
+                }
+            }
+        }
+        return answer;
+    }
+}
